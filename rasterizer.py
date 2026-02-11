2a44d217b09c8268dd43ccd6208b298bdd343fa4
SiddhanthGH <110147711+SiddhanthGH@users.noreply.github.com>
2026-02-11 01:17:14 -0500

    Added multiple triangle capability.

--- rasterizer.py
+++ rasterizer.py
@@ -1,6 +1,7 @@
 import numpy as np
 from graphics_lib import Triangle, Projector, Vertex
 import matplotlib.pyplot as plt
+from PIL import Image
 
 def getNDC(project, vertex : Vertex):
     near_point = project.toNearPlane(vertex)
@@ -25,58 +26,75 @@ h = 720
 projector = Projector(w, h, 1, 10) #width, height, near plane, far plane
 screen = np.zeros((h,w,3))
 
-#Get primitives
+x1 = [-0.8, -1.4]
+x2 = [0.8, -0.8]
+x3 = [0.0, 0.0]
 
-vertex1 = Vertex(-0.8, -0.6, -2, [1,0,0])
-vertex2 = Vertex( 0.8, -0.6, -2, [0,1,0])
-vertex3 = Vertex( 0.0,  0.6, -2, [0,0,1])
+y1 = [-0.6, -0.4]
+y2 = [-0.6, -0.6]
+y3 = [0.6, 0.6]
 
-triangle = Triangle(vertex1, vertex2, vertex3)
+z1 = [-2, -2]
+z2 = [-2, -2]
+z3 = [-2, -2]
 
-#Convert to screen space
+col1 = [[1,0,0], [0,0,1]]
+col2 = [[0,1,0], [0,1,0]]
+col3 = [[0,0,1], [1,0,0]]
 
-ss_v1 = project(projector, triangle.A)
-ss_v2 = project(projector, triangle.B)
-ss_v3 = project(projector, triangle.C) 
+for i in range(0,2):
 
-ss_tri = Triangle(ss_v1, ss_v2, ss_v3)
+    #Get primitives
 
-ss_min = ss_tri.min().floor()
-ss_max = ss_tri.max().ceil()
+    vertex1 = Vertex(x1[i], y1[i], z1[i], col1[i])
+    vertex2 = Vertex(x2[i], y2[i], z2[i], col2[i])
+    vertex3 = Vertex(x3[i], y3[i], z3[i], col3[i])
 
-#Perform edge tests
+    triangle = Triangle(vertex1, vertex2, vertex3)
 
-#print(ss_v1)
-#print(ss_v2)
-#print(ss_v3)
-#print(ss_min, ss_max)
+    #Convert to screen space
 
-#Check winding.
-area = edge(ss_tri.A, ss_tri.B, ss_tri.C.x, ss_tri.C.y) / 2 #Edge equation is cross product, so divide by 2 to get area of triangle instead of parallelogram.
-if area < 0:
-    ss_tri.B, ss_tri.C = ss_tri.C, ss_tri.B
-    area = -area
+    ss_v1 = project(projector, triangle.A)
+    ss_v2 = project(projector, triangle.B)
+    ss_v3 = project(projector, triangle.C) 
 
-for i in range(ss_min.y, ss_max.y):
-    for j in range(ss_min.x, ss_max.x):
-        x = j + 0.5
-        y = i + 0.5
+    ss_tri = Triangle(ss_v1, ss_v2, ss_v3)
 
-        edge1 = edge(ss_tri.A, ss_tri.B, x, y) / (2 * area) #Similar logic. Divide by 2 to get triangle area and then divide by area to normalize the result. The cross product above gives unnormalized barycentric coordinates. 
-        edge2 = edge(ss_tri.B, ss_tri.C, x, y) / (2 * area)
-        edge3 = edge(ss_tri.C, ss_tri.A, x, y) / (2 * area)
+    ss_min = ss_tri.min().floor()
+    ss_max = ss_tri.max().ceil()
 
-        '''
-        print(edge1)
-        print(edge2)
-        print(edge3)
-        '''
-        
-        if (edge1 >= 0 and edge2 >= 0 and edge3 >= 0):
-            screen[i][j][0] = ss_tri.A.R * edge1 + ss_tri.B.R * edge2 + ss_tri.C.R * edge3
-            screen[i][j][1] = ss_tri.A.G * edge1 + ss_tri.B.G * edge2 + ss_tri.C.G * edge3
-            screen[i][j][2] = ss_tri.A.B * edge1 + ss_tri.B.B * edge2 + ss_tri.C.B * edge3
+    #Check winding.
+    area = edge(ss_tri.A, ss_tri.B, ss_tri.C.x, ss_tri.C.y) / 2 #Edge equation is cross product, so divide by 2 to get area of triangle instead of parallelogram.
+    if area < 0:
+        ss_tri.B, ss_tri.C = ss_tri.C, ss_tri.B
+        area = -area
+
+    #Do edge testing
+    for i in range(ss_min.y, ss_max.y):
+        for j in range(ss_min.x, ss_max.x):
+            x = j + 0.5
+            y = i + 0.5
+
+            edge1 = edge(ss_tri.A, ss_tri.B, x, y) / (2 * area) #Similar logic. Divide by 2 to get triangle area and then divide by area to normalize the result. The cross product above gives unnormalized barycentric coordinates. 
+            edge2 = edge(ss_tri.B, ss_tri.C, x, y) / (2 * area)
+            edge3 = edge(ss_tri.C, ss_tri.A, x, y) / (2 * area)
+
+            '''
+            print(edge1)
+            print(edge2)
+            print(edge3)
+            '''
+            
+            if (edge1 >= 0 and edge2 >= 0 and edge3 >= 0):
+                screen[i][j][0] = ss_tri.A.R * edge1 + ss_tri.B.R * edge2 + ss_tri.C.R * edge3
+                screen[i][j][1] = ss_tri.A.G * edge1 + ss_tri.B.G * edge2 + ss_tri.C.G * edge3
+                screen[i][j][2] = ss_tri.A.B * edge1 + ss_tri.B.B * edge2 + ss_tri.C.B * edge3
 
 plt.imshow(screen)
 plt.axis('off')
 plt.show()
+
+#Save image
+img_unit8 = (screen * 255).astype(np.uint8)
+img = Image.fromarray(img_unit8)
+img.save('lossless.png', mode='RGB')
