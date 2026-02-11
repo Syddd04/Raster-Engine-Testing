a1894489facc066ac83771c2772a3ca0f7d7bf28
SiddhanthGH <110147711+SiddhanthGH@users.noreply.github.com>
2026-02-11 00:47:45 -0500

    Added RGB interpolation and new Vertex class to hold color values.

--- graphics_lib.py
+++ graphics_lib.py
@@ -2,7 +2,7 @@ import numpy as np
 from math import floor, ceil
 
 class Vec3:
-    def __init__(self, x = 0, y = 0, z = 0):
+    def __init__(self, x = 0.0, y = 0.0, z = 0.0):
         self.x = x
         self.y = y
         self.z = z
@@ -20,14 +20,24 @@ class Vec3:
         self.y = floor(self.y)
         return self
 
+    def getP(self):
+        return self
+
     def ceil(self):
         self.x = ceil(self.x)
         self.y = ceil(self.y)
         return self
 
+class Vertex(Vec3):
+    def __init__(self, x = 0.0, y = 0.0, z = 0.0, color= [0, 0, 0]):
+        super().__init__(x, y, z)
+        self.R = color[0]
+        self.G = color[1]
+        self.B = color[2]
+        self.color = color
 
 class Triangle:
-    def __init__(self, A : Vec3, B : Vec3, C : Vec3):
+    def __init__(self, A : Vertex, B : Vertex, C : Vertex):
         self.A = A
         self.B = B
         self.C = C
@@ -51,20 +61,20 @@ class Projector:
         self.far = far
         self.aspect = width / height
     
-    def toNearPlane(self, point : Vec3):
-        return Vec3((point.x * self.near) / (-point.z), (point.y * self.near) / (-point.z), point.z)
-    def toNDC(self, point : Vec3):
-        return Vec3(point.x / (self.near * self.aspect), point.y / self.near, point.z)
-    def depth(self, point : Vec3):
+    def toNearPlane(self, point : Vertex):
+        return Vertex((point.x * self.near) / (-point.z), (point.y * self.near) / (-point.z), point.z, point.color)
+    def toNDC(self, point : Vertex):
+        return Vertex(point.x / (self.near * self.aspect), point.y / self.near, point.z, point.color)
+    def depth(self, point : Vertex):
         z = point.z
         point.z = (-(self.far + self.near) / (self.far - self.near)*z) - (2*self.far*self.near / (self.near - self.far))
         return point.z
     
-    def toScreenSpace(self, ndc : Vec3):
+    def toScreenSpace(self, ndc : Vertex):
         x_ndc = ndc.x
         y_ndc = ndc.y
 
         x_pixel = (x_ndc + 1) * 0.5 * self.width
         y_pixel = (1 - y_ndc) * 0.5 * self.height
 
-        return Vec3(x_pixel, y_pixel, ndc.z)
+        return Vertex(x_pixel, y_pixel, ndc.z, ndc.color)
--- rasterizer.py
+++ rasterizer.py
@@ -1,10 +1,10 @@
 import numpy as np
-from graphics_lib import Vec3, Triangle, Projector
+from graphics_lib import Triangle, Projector, Vertex
 import matplotlib.pyplot as plt
 
-def getNDC(project, vertex : Vec3):
+def getNDC(project, vertex : Vertex):
     near_point = project.toNearPlane(vertex)
-    ndc : Vec3 = project.toNDC(near_point)
+    ndc : Vertex = project.toNDC(near_point)
     ndc.z = project.depth(ndc)
     return ndc
 
@@ -23,13 +23,13 @@ def edge(a, b, x, y):
 w = 1280
 h = 720
 projector = Projector(w, h, 1, 10) #width, height, near plane, far plane
-screen = np.zeros((h,w))
+screen = np.zeros((h,w,3))
 
 #Get primitives
 
-vertex1 = Vec3(-0.8, -0.6, -9)
-vertex2 = Vec3( 0.8, -0.6, -9)
-vertex3 = Vec3( 0.0,  0.6, -9)
+vertex1 = Vertex(-0.8, -0.6, -2, [1,0,0])
+vertex2 = Vertex( 0.8, -0.6, -2, [0,1,0])
+vertex3 = Vertex( 0.0,  0.6, -2, [0,0,1])
 
 triangle = Triangle(vertex1, vertex2, vertex3)
 
@@ -46,27 +46,37 @@ ss_max = ss_tri.max().ceil()
 
 #Perform edge tests
 
-print(ss_v1)
-print(ss_v2)
-print(ss_v3)
-print(ss_min, ss_max)
+#print(ss_v1)
+#print(ss_v2)
+#print(ss_v3)
+#print(ss_min, ss_max)
 
-area = edge(ss_tri.A, ss_tri.B, ss_tri.C.x, ss_tri.C.y)
+#Check winding.
+area = edge(ss_tri.A, ss_tri.B, ss_tri.C.x, ss_tri.C.y) / 2 #Edge equation is cross product, so divide by 2 to get area of triangle instead of parallelogram.
 if area < 0:
     ss_tri.B, ss_tri.C = ss_tri.C, ss_tri.B
+    area = -area
 
 for i in range(ss_min.y, ss_max.y):
     for j in range(ss_min.x, ss_max.x):
         x = j + 0.5
         y = i + 0.5
 
-        edge1 = edge(ss_tri.A, ss_tri.B, x, y)
-        edge2 = edge(ss_tri.B, ss_tri.C, x, y)
-        edge3 = edge(ss_tri.C, ss_tri.A, x, y)
+        edge1 = edge(ss_tri.A, ss_tri.B, x, y) / (2 * area) #Similar logic. Divide by 2 to get triangle area and then divide by area to normalize the result. The cross product above gives unnormalized barycentric coordinates. 
+        edge2 = edge(ss_tri.B, ss_tri.C, x, y) / (2 * area)
+        edge3 = edge(ss_tri.C, ss_tri.A, x, y) / (2 * area)
+
+        '''
+        print(edge1)
+        print(edge2)
+        print(edge3)
+        '''
         
         if (edge1 >= 0 and edge2 >= 0 and edge3 >= 0):
-            screen[i][j] = 1
+            screen[i][j][0] = ss_tri.A.R * edge1 + ss_tri.B.R * edge2 + ss_tri.C.R * edge3
+            screen[i][j][1] = ss_tri.A.G * edge1 + ss_tri.B.G * edge2 + ss_tri.C.G * edge3
+            screen[i][j][2] = ss_tri.A.B * edge1 + ss_tri.B.B * edge2 + ss_tri.C.B * edge3
 
-plt.imshow(screen, cmap='gray', interpolation='nearest')
+plt.imshow(screen)
 plt.axis('off')
 plt.show()
