0f9aa8ae2e27ea55114f77dedd88387d93a7afe0
SydGH <110147711+SiddhanthGH@users.noreply.github.com>
2026-02-11 12:14:51 -0500

    Added 2x MSAA.

--- rasterizer.py
+++ rasterizer.py
@@ -21,18 +21,20 @@ def edge(a, b, x, y):
 
 #Set up machines
 
-w = 1280
-h = 720
+msaa : int = 0
+w = 640
+h = 480
 projector = Projector(w, h, 1, 10) #width, height, near plane, far plane
 screen = np.zeros((h,w,3))
+coverage = np.zeros((h, w*msaa))
 
-x1 = [-0.8, -1.4]
-x2 = [0.8, -0.8]
-x3 = [0.0, 0.0]
+vx1 = [-0.8, -1.4]
+vx2 = [0.8, -0.8]
+vx3 = [0.0, 0.0]
 
-y1 = [-0.6, -0.4]
-y2 = [-0.6, -0.6]
-y3 = [0.6, 0.6]
+vy1 = [-0.6, -0.4]
+vy2 = [-0.6, -0.6]
+vy3 = [0.6, 0.6]
 
 z1 = [-2, -2]
 z2 = [-2, -2]
@@ -46,9 +48,9 @@ for i in range(0,2):
 
     #Get primitives
 
-    vertex1 = Vertex(x1[i], y1[i], z1[i], col1[i])
-    vertex2 = Vertex(x2[i], y2[i], z2[i], col2[i])
-    vertex3 = Vertex(x3[i], y3[i], z3[i], col3[i])
+    vertex1 = Vertex(vx1[i], vy1[i], z1[i], col1[i])
+    vertex2 = Vertex(vx2[i], vy2[i], z2[i], col2[i])
+    vertex3 = Vertex(vx3[i], vy3[i], z3[i], col3[i])
 
     triangle = Triangle(vertex1, vertex2, vertex3)
 
@@ -70,31 +72,57 @@ for i in range(0,2):
         area = -area
 
     #Do edge testing
-    for i in range(ss_min.y, ss_max.y):
+    for y in range(ss_min.y, ss_max.y):
         for j in range(ss_min.x, ss_max.x):
-            x = j + 0.5
-            y = i + 0.5
+            bx = j + 0.5
+            by = y + 0.5
 
-            edge1 = edge(ss_tri.A, ss_tri.B, x, y) / (2 * area) #Similar logic. Divide by 2 to get triangle area and then divide by area to normalize the result. The cross product above gives unnormalized barycentric coordinates. 
-            edge2 = edge(ss_tri.B, ss_tri.C, x, y) / (2 * area)
-            edge3 = edge(ss_tri.C, ss_tri.A, x, y) / (2 * area)
+            edge1 = edge(ss_tri.A, ss_tri.B, bx, by) / (2 * area) #Similar logic. Divide by 2 to get triangle area and then divide by area to normalize the result. The cross product above gives unnormalized barycentric coordinates. 
+            edge2 = edge(ss_tri.B, ss_tri.C, bx, by) / (2 * area)
+            edge3 = edge(ss_tri.C, ss_tri.A, bx, by) / (2 * area)
 
             '''
             print(edge1)
             print(edge2)
             print(edge3)
             '''
+
+            if (msaa == 2):
+                x0 = j + 0.25
+                x1 = j + 0.75
+
+                y0 = y + 0.25
+                y1 = y + 0.75
+
+                e0_1 = edge(ss_tri.A, ss_tri.B, x0, y0)
+                e0_2 = edge(ss_tri.B, ss_tri.C, x0, y0)
+                e0_3 = edge(ss_tri.C, ss_tri.A, x0, y0)
+
+                e1_1 = edge(ss_tri.A, ss_tri.B, x1, y1)
+                e1_2 = edge(ss_tri.B, ss_tri.C, x1, y1)
+                e1_3 = edge(ss_tri.C, ss_tri.A, x1, y1)
+
+                if (e0_1 >= 0 and e0_2 >= 0 and e0_3 >= 0):
+                    coverage[y][j + 0] = 1
+                if (e1_1 >= 0 and e1_2 >= 0 and e1_3 >= 0):
+                    coverage[y][j + 1] = 1
             
             if (edge1 >= 0 and edge2 >= 0 and edge3 >= 0):
-                screen[i][j][0] = ss_tri.A.R * edge1 + ss_tri.B.R * edge2 + ss_tri.C.R * edge3
-                screen[i][j][1] = ss_tri.A.G * edge1 + ss_tri.B.G * edge2 + ss_tri.C.G * edge3
-                screen[i][j][2] = ss_tri.A.B * edge1 + ss_tri.B.B * edge2 + ss_tri.C.B * edge3
+                if (msaa == 2):
+                    screen[y][j][0] = (((ss_tri.A.R * edge1 + ss_tri.B.R * edge2 + ss_tri.C.R * edge3) * coverage[y][j]) + ((ss_tri.A.R * edge1 + ss_tri.B.R * edge2 + ss_tri.C.R * edge3) * coverage[y][j + 1])) / 2
+                    screen[y][j][1] = (((ss_tri.A.G * edge1 + ss_tri.B.G * edge2 + ss_tri.C.G * edge3) * coverage[y][j]) + ((ss_tri.A.G * edge1 + ss_tri.B.G * edge2 + ss_tri.C.G * edge3) * coverage[y][j + 1])) / 2
+                    screen[y][j][2] = (((ss_tri.A.B * edge1 + ss_tri.B.B * edge2 + ss_tri.C.B * edge3) * coverage[y][j]) + ((ss_tri.A.B * edge1 + ss_tri.B.B * edge2 + ss_tri.C.B * edge3) * coverage[y][j + 1])) / 2
+                else:
+                    screen[y][j][0] = ss_tri.A.R * edge1 + ss_tri.B.R * edge2 + ss_tri.C.R * edge3
+                    screen[y][j][1] = ss_tri.A.G * edge1 + ss_tri.B.G * edge2 + ss_tri.C.G * edge3
+                    screen[y][j][2] = ss_tri.A.B * edge1 + ss_tri.B.B * edge2 + ss_tri.C.B * edge3
 
 plt.imshow(screen)
 plt.axis('off')
 plt.show()
 
 #Save image
+'''
 img_unit8 = (screen * 255).astype(np.uint8)
 img = Image.fromarray(img_unit8)
-img.save('lossless.png', mode='RGB')
+img.save('lossless.png', mode='RGB')'''
