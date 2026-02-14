fc6656c0deb99b97ebe5226cfe458a75f7425768
SiddhanthGH <110147711+SiddhanthGH@users.noreply.github.com>
2026-02-14 17:44:02 -0500

    Bring msaa even closer to hardware model. Default resolution bumped due to (expected) artifacts through hardware optimized algorithm.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from rasterizer import Rasterizer
 from time import perf_counter
 
-'''
+
 v0 = (-0.8, -0.8, -2)  # bottom left
 v1 = ( 0.8, -0.8, -2)  # bottom right
 v2 = ( 0.8,  0.8, -2)  # top right
@@ -28,8 +28,8 @@ vz3 = [v3[2], v2[2]]
 col1 = [c0, c1]
 col2 = [c1, c3]
 col3 = [c3, c2]
-'''
 
+'''
 vx1 = [-0.8, -1.4, 0.8]
 vy1 = [-0.6, -0.4, -0.6]
 vz1  = [-2,   -2,   -2]
@@ -45,14 +45,15 @@ vz3  = [-2,  -2,  -3]
 col1 = [[1,0,0], [0,0,1], [1,1,1]]
 col2 = [[0,1,0], [0,1,0], [1,1,1]]
 col3 = [[0,0,1], [1,0,0], [1,1,1]]
+'''
 
 rasterEngine = Rasterizer(
     vx1, vy1, vz1,
     vx2, vy2, vz2,
     vx3, vy3, vz3,
     col1, col2, col3,
-    msaa=2, w=854, h=480
-)
+    msaa=2
+) #defaults to 720p
 
 start = perf_counter()
 rasterEngine.render()
--- rasterizer.py
+++ rasterizer.py
@@ -33,7 +33,7 @@ class Rasterizer():
     def de_dy(self, a, b):
         return b.x - a.x
 
-    def __init__(self, vx1, vy1, vz1, vx2, vy2, vz2, vx3, vy3, vz3, col1 = [[]], col2 = [[]], col3 = [[]], u1 = [], u2 = [], u3 = [], v1 = [], v2 = [], v3 = [], msaa = 0, w = 854, h = 480, near = 1, far = 10):
+    def __init__(self, vx1, vy1, vz1, vx2, vy2, vz2, vx3, vy3, vz3, col1 = [[]], col2 = [[]], col3 = [[]], u1 = [], u2 = [], u3 = [], v1 = [], v2 = [], v3 = [], msaa = 0, w=1280, h=720, near = 1, far = 10):
         msaa = 2 if (msaa > 2) else msaa
         
         #numpy arrays, all of them. Cols will be nx3x3 array
@@ -133,21 +133,21 @@ class Rasterizer():
                 de1_dy2 = self.de_dy(ss_tri.C, ss_tri.A) * norm_factor
                 de1_dy3 = self.de_dy(ss_tri.A, ss_tri.B) * norm_factor
                 
-            elif (self.msaa == 0):
-                bx = ss_min.x + 0.5
-                by = ss_min.y + 0.5
-                edge1 = self.edge(ss_tri.B, ss_tri.C, bx, by) * norm_factor #Similar logic. Divide by 2 to get triangle area and then divide by area to normalize the result. The cross product above gives unnormalized barycentric coordinates. 
-                edge2 = self.edge(ss_tri.C, ss_tri.A, bx, by) * norm_factor
-                edge3 = self.edge(ss_tri.A, ss_tri.B, bx, by) * norm_factor
-                de_dx1 = self.de_dx(ss_tri.B, ss_tri.C) * norm_factor
-                de_dx2 = self.de_dx(ss_tri.C, ss_tri.A) * norm_factor
-                de_dx3 = self.de_dx(ss_tri.A, ss_tri.B) * norm_factor
-                de_dy1 = self.de_dy(ss_tri.B, ss_tri.C) * norm_factor
-                de_dy2 = self.de_dy(ss_tri.C, ss_tri.A) * norm_factor
-                de_dy3 = self.de_dy(ss_tri.A, ss_tri.B) * norm_factor
-                e_ini1 = edge1
-                e_ini2 = edge2
-                e_ini3 = edge3
+            
+            bx = ss_min.x + 0.5
+            by = ss_min.y + 0.5
+            edge1 = self.edge(ss_tri.B, ss_tri.C, bx, by) * norm_factor #Similar logic. Divide by 2 to get triangle area and then divide by area to normalize the result. The cross product above gives unnormalized barycentric coordinates. 
+            edge2 = self.edge(ss_tri.C, ss_tri.A, bx, by) * norm_factor
+            edge3 = self.edge(ss_tri.A, ss_tri.B, bx, by) * norm_factor
+            de_dx1 = self.de_dx(ss_tri.B, ss_tri.C) * norm_factor
+            de_dx2 = self.de_dx(ss_tri.C, ss_tri.A) * norm_factor
+            de_dx3 = self.de_dx(ss_tri.A, ss_tri.B) * norm_factor
+            de_dy1 = self.de_dy(ss_tri.B, ss_tri.C) * norm_factor
+            de_dy2 = self.de_dy(ss_tri.C, ss_tri.A) * norm_factor
+            de_dy3 = self.de_dy(ss_tri.A, ss_tri.B) * norm_factor
+            e_ini1 = edge1
+            e_ini2 = edge2
+            e_ini3 = edge3
 
             check1 = 0 if self.validEdge(ss_tri.B, ss_tri.C) else -1e-12 #adjust for subtle fp errors
             check2 = 0 if self.validEdge(ss_tri.C, ss_tri.A) else -1e-12
@@ -163,10 +163,10 @@ class Rasterizer():
                     e1_1 = e1_1_ini
                     e1_2 = e1_2_ini
                     e1_3 = e1_3_ini
-                else:
-                    edge1 = e_ini1
-                    edge2 = e_ini2
-                    edge3 = e_ini3
+                
+                edge1 = e_ini1
+                edge2 = e_ini2
+                edge3 = e_ini3
                     
                 for j in range(ss_min.x, ss_max.x):
                     if area == 0:
@@ -185,7 +185,10 @@ class Rasterizer():
                         z0 = (ss_tri.A.z * e0_1 + ss_tri.B.z * e0_2 + ss_tri.C.z * e0_3)
                         z1 = (ss_tri.A.z * e1_1 + ss_tri.B.z * e1_2 + ss_tri.C.z * e1_3)
 
-                        if ((cov0 and z0 <= self.z_buffer[y][j][0]) or (cov1 and z1 <= self.z_buffer[y][j][1])):
+                        pass0 = (cov0 and z0 <= self.z_buffer[y][j][0])
+                        pass1 = (cov1 and z1 <= self.z_buffer[y][j][1])
+
+                        if (pass0 or pass1):
                             r0 = self.color_buffer[y][j][0][0]
                             g0 = self.color_buffer[y][j][0][1]
                             b0 = self.color_buffer[y][j][0][2]
@@ -194,29 +197,35 @@ class Rasterizer():
                             g1 = self.color_buffer[y][j][1][1]
                             b1 = self.color_buffer[y][j][1][2]
 
-                            if (z0 <= self.z_buffer[y][j][0] and cov0):
+                            
+                            accum1 = ss_tri.A.R * edge1 + ss_tri.B.R * edge2 + ss_tri.C.R * edge3
+                            accum2 = ss_tri.A.G * edge1 + ss_tri.B.G * edge2 + ss_tri.C.G * edge3
+                            accum3 = ss_tri.A.B * edge1 + ss_tri.B.B * edge2 + ss_tri.C.B * edge3
+
+                            if (pass0 and not pass1):
+                                accum1 = ((ss_tri.A.R * e0_1 + ss_tri.B.R * e0_2 + ss_tri.C.R * e0_3))
+                                accum2 = ((ss_tri.A.G * e0_1 + ss_tri.B.G * e0_2 + ss_tri.C.G * e0_3))
+                                accum3 = ((ss_tri.A.B * e0_1 + ss_tri.B.B * e0_2 + ss_tri.C.B * e0_3))
+                            elif (not pass0 and pass1):
+                                accum1 = ((ss_tri.A.R * e1_1 + ss_tri.B.R * e1_2 + ss_tri.C.R * e1_3))
+                                accum2 = ((ss_tri.A.G * e1_1 + ss_tri.B.G * e1_2 + ss_tri.C.G * e1_3))
+                                accum3 = ((ss_tri.A.B * e1_1 + ss_tri.B.B * e1_2 + ss_tri.C.B * e1_3))
+
+                            if (pass0):
                                 self.z_buffer[y][j][0] = z0
-                                r0 = ((ss_tri.A.R * e0_1 + ss_tri.B.R * e0_2 + ss_tri.C.R * e0_3))
-                                g0 = ((ss_tri.A.G * e0_1 + ss_tri.B.G * e0_2 + ss_tri.C.G * e0_3))
-                                b0 = ((ss_tri.A.B * e0_1 + ss_tri.B.B * e0_2 + ss_tri.C.B * e0_3))
-                            elif(cov0):
-                                r0 = self.color_buffer[y][j][0][0]
-                                g0 = self.color_buffer[y][j][0][1]
-                                b0 = self.color_buffer[y][j][0][2]
+                                r0 = accum1
+                                g0 = accum2
+                                b0 = accum3
 
                             self.color_buffer[y][j][0][0] = r0
                             self.color_buffer[y][j][0][1] = g0
                             self.color_buffer[y][j][0][2] = b0
 
-                            if (z1 <= self.z_buffer[y][j][1] and cov1):
+                            if (pass1):
                                 self.z_buffer[y][j][1] = z1
-                                r1 = ((ss_tri.A.R * e1_1 + ss_tri.B.R * e1_2 + ss_tri.C.R * e1_3))
-                                g1 = ((ss_tri.A.G * e1_1 + ss_tri.B.G * e1_2 + ss_tri.C.G * e1_3))
-                                b1 = ((ss_tri.A.B * e1_1 + ss_tri.B.B * e1_2 + ss_tri.C.B * e1_3))
-                            elif (cov1):
-                                r1 = self.color_buffer[y][j][1][0]
-                                g1 = self.color_buffer[y][j][1][1]
-                                b1 = self.color_buffer[y][j][1][2]
+                                r1 = accum1
+                                g1 = accum2
+                                b1 = accum3
 
                             self.color_buffer[y][j][1][0] = r1
                             self.color_buffer[y][j][1][1] = g1
@@ -243,10 +252,10 @@ class Rasterizer():
                         e1_1 += de1_dx1
                         e1_2 += de1_dx2
                         e1_3 += de1_dx3
-                    else:
-                        edge1 += de_dx1
-                        edge2 += de_dx2
-                        edge3 += de_dx3
+                    
+                    edge1 += de_dx1
+                    edge2 += de_dx2
+                    edge3 += de_dx3
                 
                 if (self.msaa == 2):
                     e0_1_ini += de0_dy1
@@ -255,10 +264,10 @@ class Rasterizer():
                     e1_1_ini += de1_dy1
                     e1_2_ini += de1_dy2
                     e1_3_ini += de1_dy3
-                else:
-                    e_ini1 += de_dy1
-                    e_ini2 += de_dy2
-                    e_ini3 += de_dy3
+                
+                e_ini1 += de_dy1
+                e_ini2 += de_dy2
+                e_ini3 += de_dy3
                     
                     
 
